760d1ccb2e621b2fe5225bb9b37946ec3e6845ee
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-02-28 15:42:38 +0100

    Athena: Add RAM and storage info
    Change-Id: I98b63ed560218f38044b7d13af03400ada30fac9

--- app/src/main/java/dev/sebaubuntu/athena/categories/DeviceCategory.kt
+++ app/src/main/java/dev/sebaubuntu/athena/categories/DeviceCategory.kt
@@ -5,9 +5,13 @@
 
 package dev.sebaubuntu.athena.categories
 
+import android.app.ActivityManager
 import android.content.Context
 import android.os.Build
+import android.os.Environment
+import android.os.StatFs
 import dev.sebaubuntu.athena.R
+import dev.sebaubuntu.athena.utils.BytesUtils
 import dev.sebaubuntu.athena.utils.Category
 
 object DeviceCategory : Category {
@@ -17,6 +21,23 @@ object DeviceCategory : Category {
     override val requiredPermissions = arrayOf<String>()
 
     override fun getInfo(context: Context) = mutableMapOf<String, Map<String, String>>().apply {
+        val activityManager = context.getSystemService(ActivityManager::class.java)
+        val memoryInfo = ActivityManager.MemoryInfo().apply {
+            activityManager.getMemoryInfo(this)
+        }
+
+        val internalStatFs = StatFs(Environment.getDataDirectory().absolutePath)
+        val internalTotal =
+            internalStatFs.blockCountLong * internalStatFs.blockSizeLong
+        val internalFree =
+            internalStatFs.availableBlocksLong * internalStatFs.blockSizeLong
+
+        val externalStatFs = StatFs(Environment.getExternalStorageDirectory().absolutePath)
+        val externalTotal =
+            externalStatFs.blockCountLong * externalStatFs.blockSizeLong
+        val externalFree =
+            externalStatFs.availableBlocksLong * externalStatFs.blockSizeLong
+
         this["General"] = mutableMapOf(
             "Device" to Build.DEVICE,
             "Brand" to Build.BRAND,
@@ -40,5 +61,25 @@ object DeviceCategory : Category {
                 "Model" to Build.SOC_MODEL,
             )
         }
+
+        this["RAM"] = mapOf(
+            "Total memory" to BytesUtils.toHumanReadableSIPrefixes(memoryInfo.totalMem),
+            "Available memory" to BytesUtils.toHumanReadableSIPrefixes(memoryInfo.availMem),
+            "Is a low memory system" to "${memoryInfo.lowMemory}",
+        )
+
+        this["Internal storage"] = mapOf(
+            "Total" to BytesUtils.toHumanReadableSIPrefixes(internalTotal),
+            "Available" to BytesUtils.toHumanReadableSIPrefixes(internalFree),
+            "Used" to BytesUtils.toHumanReadableSIPrefixes(internalTotal - internalFree),
+        )
+
+        this["External storage"] = mapOf(
+            "Is emulated" to "${Environment.isExternalStorageEmulated()}",
+            "Is removable" to "${Environment.isExternalStorageRemovable()}",
+            "Total" to BytesUtils.toHumanReadableSIPrefixes(externalTotal),
+            "Available" to BytesUtils.toHumanReadableSIPrefixes(externalFree),
+            "Used" to BytesUtils.toHumanReadableSIPrefixes(externalTotal - externalFree),
+        )
     }.toMap()
 }
