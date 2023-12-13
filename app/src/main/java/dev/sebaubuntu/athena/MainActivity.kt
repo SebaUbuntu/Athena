a308d84596e8eeed117714f9f4881ba329382129
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-12-13 15:38:35 +0100

    Athena: Enable edge-to-edge
    Change-Id: Ie24fde56d367e09adb03723ecc72e4c5f066d524

--- app/src/main/java/dev/sebaubuntu/athena/MainActivity.kt
+++ app/src/main/java/dev/sebaubuntu/athena/MainActivity.kt
@@ -6,7 +6,13 @@
 package dev.sebaubuntu.athena
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -14,10 +20,12 @@ import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     // Views
+    private val contentView by lazy { findViewById<View>(android.R.id.content) }
+    private val fragmentContainerView by lazy { findViewById<FragmentContainerView>(R.id.fragmentContainerView) }
     private val toolbar by lazy { findViewById<MaterialToolbar>(R.id.toolbar) }
 
     // Fragments
-    private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment }
+    private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment }
 
     private val navController by lazy { navHostFragment.navController }
 
@@ -26,7 +34,22 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        // Setup edge-to-edge
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        ViewCompat.setOnApplyWindowInsetsListener(contentView) { _, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            fragmentContainerView.updatePadding(
+                bottom = insets.bottom,
+                left = insets.left,
+                right = insets.right,
+            )
+
+            windowInsets
+        }
     }
 }
