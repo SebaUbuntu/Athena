204241c23cebd92084d1fbe13454b4efd7a950e0
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-12-18 21:55:58 +0100

    Athena: ListItem: Add set text/image utils
    Change-Id: I5acff272307d8361582a1a52e3326c2c6ef749a0

--- app/src/main/java/dev/sebaubuntu/athena/ui/ListItem.kt
+++ app/src/main/java/dev/sebaubuntu/athena/ui/ListItem.kt
@@ -6,11 +6,16 @@
 package dev.sebaubuntu.athena.ui
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.Icon
+import android.net.Uri
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.core.view.isVisible
 import dev.sebaubuntu.athena.R
 
@@ -30,32 +35,31 @@ class ListItem @JvmOverloads constructor(
     var headlineText: CharSequence?
         get() = headlineTextView.text
         set(value) {
-            headlineTextView.text = value
-            headlineTextView.isVisible = !value.isNullOrEmpty()
+            headlineTextView.setTextAndUpdateVisibility(value)
         }
+
     var leadingIconImage: Drawable?
         get() = leadingIconImageView.drawable
         set(value) {
-            leadingIconImageView.setImageDrawable(value)
-            leadingIconImageView.isVisible = value != null
+            leadingIconImageView.setImageAndUpdateVisibility(value)
         }
+
     var supportingText: CharSequence?
         get() = supportingTextView.text
         set(value) {
-            supportingTextView.text = value
-            supportingTextView.isVisible = !value.isNullOrEmpty()
+            supportingTextView.setTextAndUpdateVisibility(value)
         }
+
     var trailingIconImage: Drawable?
         get() = trailingIconImageView.drawable
         set(value) {
-            trailingIconImageView.setImageDrawable(value)
-            trailingIconImageView.isVisible = value != null
+            trailingIconImageView.setImageAndUpdateVisibility(value)
         }
+
     var trailingSupportingText: CharSequence?
         get() = trailingSupportingTextView.text
         set(value) {
-            trailingSupportingTextView.text = value
-            trailingSupportingTextView.isVisible = !value.isNullOrEmpty()
+            trailingSupportingTextView.setTextAndUpdateVisibility(value)
         }
 
     init {
@@ -73,4 +77,71 @@ class ListItem @JvmOverloads constructor(
             }
         }
     }
+
+    fun setHeadlineText(@StringRes resId: Int) = headlineTextView.setTextAndUpdateVisibility(resId)
+    fun setHeadlineText(@StringRes resId: Int, vararg formatArgs: Any) =
+        headlineTextView.setTextAndUpdateVisibility(resId, *formatArgs)
+
+    fun setLeadingIconImage(bm: Bitmap) = leadingIconImageView.setImageAndUpdateVisibility(bm)
+    fun setLeadingIconImage(icon: Icon) = leadingIconImageView.setImageAndUpdateVisibility(icon)
+    fun setLeadingIconImage(@DrawableRes resId: Int) =
+        leadingIconImageView.setImageAndUpdateVisibility(resId)
+    fun setLeadingIconImage(uri: Uri) = leadingIconImageView.setImageAndUpdateVisibility(uri)
+
+    fun setSupportingText(@StringRes resId: Int) =
+        supportingTextView.setTextAndUpdateVisibility(resId)
+    fun setSupportingText(@StringRes resId: Int, vararg formatArgs: Any) =
+        supportingTextView.setTextAndUpdateVisibility(resId, *formatArgs)
+
+    fun setTrailingIconImage(bm: Bitmap) = trailingIconImageView.setImageAndUpdateVisibility(bm)
+    fun setTrailingIconImage(icon: Icon) = trailingIconImageView.setImageAndUpdateVisibility(icon)
+    fun setTrailingIconImage(@DrawableRes resId: Int) =
+        trailingIconImageView.setImageAndUpdateVisibility(resId)
+    fun setTrailingIconImage(uri: Uri) = trailingIconImageView.setImageAndUpdateVisibility(uri)
+
+    fun setTrailingSupportingText(@StringRes resId: Int) =
+        trailingSupportingTextView.setTextAndUpdateVisibility(resId)
+    fun setTrailingSupportingText(@StringRes resId: Int, vararg formatArgs: Any) =
+        trailingSupportingTextView.setTextAndUpdateVisibility(resId, *formatArgs)
+
+    // ImageView utils
+
+    private fun ImageView.setImageAndUpdateVisibility(bm: Bitmap) {
+        setImageBitmap(bm)
+        isVisible = true
+    }
+
+    private fun ImageView.setImageAndUpdateVisibility(drawable: Drawable?) {
+        setImageDrawable(drawable)
+        isVisible = drawable != null
+    }
+
+    private fun ImageView.setImageAndUpdateVisibility(icon: Icon) {
+        setImageIcon(icon)
+        isVisible = true
+    }
+
+    private fun ImageView.setImageAndUpdateVisibility(@DrawableRes resId: Int) {
+        setImageResource(resId)
+        isVisible = true
+    }
+
+    private fun ImageView.setImageAndUpdateVisibility(uri: Uri) {
+        setImageURI(uri)
+        isVisible = true
+    }
+
+    // TextView utils
+
+    private fun TextView.setTextAndUpdateVisibility(text: CharSequence?) {
+        this.text = text.also {
+            isVisible = !it.isNullOrEmpty()
+        }
+    }
+
+    private fun TextView.setTextAndUpdateVisibility(@StringRes resId: Int) =
+        setTextAndUpdateVisibility(resources.getText(resId))
+
+    private fun TextView.setTextAndUpdateVisibility(@StringRes resId: Int, vararg formatArgs: Any) =
+        setTextAndUpdateVisibility(resources.getString(resId, *formatArgs))
 }
