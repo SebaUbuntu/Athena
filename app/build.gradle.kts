5e3b457f140072395fd8bd397f4621904bea2633
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-12-25 04:23:52 +0100

    Athena: Update libs
    Change-Id: I51ea89882de1c4a1490054205e8b37f895d03f4d

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,12 +51,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
 
     // Biometrics
     implementation("androidx.biometric:biometric:1.2.0-alpha05")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 }
