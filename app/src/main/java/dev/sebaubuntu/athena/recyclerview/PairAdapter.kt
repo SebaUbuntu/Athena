efc006a915a4e4f9fe420c89f2a0974fc77452ac
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-03-26 14:37:22 +0100

    Athena: PairAdapter: Handle null values
    Change-Id: Ibc86aa8e3899ccce55a37d5fffef7057c65cbb64

--- app/src/main/java/dev/sebaubuntu/athena/recyclerview/PairAdapter.kt
+++ app/src/main/java/dev/sebaubuntu/athena/recyclerview/PairAdapter.kt
@@ -6,26 +6,29 @@
 package dev.sebaubuntu.athena.recyclerview
 
 import androidx.recyclerview.widget.DiffUtil
+import dev.sebaubuntu.athena.R
 import dev.sebaubuntu.athena.ui.views.ListItem
 
-class PairAdapter : SimpleListAdapter<Pair<String, String>, ListItem>(
+class PairAdapter : SimpleListAdapter<Pair<String, String?>, ListItem>(
     diffCallback, ListItem::class.java
 ) {
-    override fun ViewHolder.onBindView(item: Pair<String, String>) {
+    override fun ViewHolder.onBindView(item: Pair<String, String?>) {
         view.headlineText = item.first
-        view.supportingText = item.second
+        item.second?.let {
+            view.supportingText = item.second
+        } ?: view.setSupportingText(R.string.unknown)
     }
 
     companion object {
-        val diffCallback = object : DiffUtil.ItemCallback<Pair<String, String>>() {
+        val diffCallback = object : DiffUtil.ItemCallback<Pair<String, String?>>() {
             override fun areItemsTheSame(
-                oldItem: Pair<String, String>,
-                newItem: Pair<String, String>
+                oldItem: Pair<String, String?>,
+                newItem: Pair<String, String?>
             ) = oldItem.first == newItem.first
 
             override fun areContentsTheSame(
-                oldItem: Pair<String, String>,
-                newItem: Pair<String, String>
+                oldItem: Pair<String, String?>,
+                newItem: Pair<String, String?>
             ) = oldItem.second == newItem.second
         }
     }
