4b9899154a749a979e508eacb1fa238de141cacd
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-04-03 21:52:34 +0200

    Athena: Move back thermal section to data flow
    Change-Id: Ia6010dcb308bbe7a66a377e6bef83194850284a5

--- app/src/main/java/dev/sebaubuntu/athena/sections/ThermalSection.kt
+++ app/src/main/java/dev/sebaubuntu/athena/sections/ThermalSection.kt
@@ -1,17 +1,80 @@
 /*
- * SPDX-FileCopyrightText: 2023 Sebastiano Barezzi
+ * SPDX-FileCopyrightText: 2023-2024 Sebastiano Barezzi
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package dev.sebaubuntu.athena.sections
 
+import android.content.Context
+import android.os.Build
+import android.os.PowerManager
+import androidx.annotation.RequiresApi
 import dev.sebaubuntu.athena.R
+import dev.sebaubuntu.athena.ext.thermalStatusFlow
+import dev.sebaubuntu.athena.models.data.Information
+import dev.sebaubuntu.athena.models.data.InformationValue
 import dev.sebaubuntu.athena.models.data.Section
+import dev.sebaubuntu.athena.models.data.Subsection
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.collectLatest
 
 object ThermalSection : Section() {
     override val title = R.string.section_thermal_name
     override val description = R.string.section_thermal_description
     override val icon = R.drawable.ic_thermostat
 
-    override val navigationActionId = R.id.action_mainFragment_to_thermalFragment
+    override fun dataFlow(context: Context) = channelFlow {
+        var thermalStatus: Int? = null
+
+        val updateData = {
+            trySend(
+                listOf(
+                    Subsection(
+                        "general",
+                        listOf(
+                            Information(
+                                "thermal_status",
+                                thermalStatus?.let {
+                                    InformationValue.IntValue(
+                                        it,
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                                            thermalStatusToStringResId
+                                        } else {
+                                            null
+                                        }
+                                    )
+                                } ?: InformationValue.StringResValue(
+                                    R.string.thermal_status_unsupported,
+                                    null,
+                                    "unsupported",
+                                ),
+                                R.string.thermal_status,
+                            )
+                        ),
+                        R.string.thermal_general,
+                    )
+                )
+            )
+        }
+
+        updateData()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            context.getSystemService(PowerManager::class.java).thermalStatusFlow().collectLatest {
+                thermalStatus = it
+                updateData()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private val thermalStatusToStringResId = mapOf(
+        PowerManager.THERMAL_STATUS_NONE to R.string.thermal_status_none,
+        PowerManager.THERMAL_STATUS_LIGHT to R.string.thermal_status_light,
+        PowerManager.THERMAL_STATUS_MODERATE to R.string.thermal_status_moderate,
+        PowerManager.THERMAL_STATUS_SEVERE to R.string.thermal_status_severe,
+        PowerManager.THERMAL_STATUS_CRITICAL to R.string.thermal_status_critical,
+        PowerManager.THERMAL_STATUS_EMERGENCY to R.string.thermal_status_emergency,
+        PowerManager.THERMAL_STATUS_SHUTDOWN to R.string.thermal_status_shutdown,
+    )
 }
