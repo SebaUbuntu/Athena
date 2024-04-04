25a118fdceb9dbd801217a52e674bcdbf82a8b6d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-04-04 12:25:53 +0200

    Athena: Update Kotlin to 1.8.22
    Change-Id: I0e2c6e6e21967c4b8ad1a1a00e4bb3c00596f794

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 plugins {
     id("com.android.application") version "8.3.1" apply false
     id("com.android.library") version "8.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.22" apply false
 }
 
 tasks.register<Delete>("clean").configure {
