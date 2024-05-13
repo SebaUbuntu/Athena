c0a9d0f1c27e6c35fb7ba0c530016c7b7dfd8ad4
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-05-13 17:14:52 +0200

    Athena: v0.2.1
    Change-Id: I1aa76d236fbcd4f51a3adb664906fc37c5750858

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.sebaubuntu.athena"
         minSdk = 23
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
     }
 
     buildTypes {
