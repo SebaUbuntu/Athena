c3be5b5fb27b55574308539d9c1f61b23257a832
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-01 17:45:59 +0200

    Athena: v0.5.0 (7)
    Change-Id: I2575a9850fd7e04bcb46cd6cd9010097b82ecc13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.sebaubuntu.athena"
         minSdk = 23
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.4.1"
+        versionCode = 7
+        versionName = "0.5.0"
 
         externalNativeBuild {
             cmake {
