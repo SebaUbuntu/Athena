c486d2a35d6166c784e549e76cba594f81095232
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-11-11 23:26:08 +0100

    Athena: Update dependencies
    Change-Id: I80c3b66e02a7e0674f74d53e5201656996ed26e1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,26 +62,26 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("com.google.android.material:material:1.12.0")
 
     // Biometrics
     implementation("androidx.biometric:biometric:1.2.0-alpha05")
 
     // LiveData
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
 
     // Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 
     // UWB
-    implementation("androidx.core.uwb:uwb:1.0.0-alpha08")
+    implementation("androidx.core.uwb:uwb:1.0.0-alpha09")
 
     // Security
     implementation("androidx.security:security-state:1.0.0-alpha04")
