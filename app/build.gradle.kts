af29e93c737fd4aaf7596e268be2778069ce88cf
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-10-18 00:03:34 +0200

    Athena: v2.0.3 (17)
    Change-Id: Ib8fd394635a72ce39c86958f0e799a8642b64038

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "dev.sebaubuntu.athena"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 16
-        versionName = "2.0.2"
+        versionCode = 17
+        versionName = "2.0.3"
     }
 
     dependenciesInfo {
